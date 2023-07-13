c9a8cdc0a5f8311b146f5bdfb6c231997fb18a16
FYClamityDestiny <99613329+FYClamityDestiny@users.noreply.github.com>
2023-07-13 08:17:00 -0400

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,3 +1,22 @@
 username = localStorage.getItem("user");
-console.log(username)
-document.getElementById("username").innerHTML = username;
+console.log(username);
+document.getElementById("userName").innerHTML = "Hi " +  username + "!";
+function addRoom(){
+roomname = document.getElementById("roomName").innerHTML;
+localStorage.setItem("roomname",roomname);
+window.location = "kwitter_page.html";
+}
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+row = "<div class='room_name' id="+roomnames+" onclick='redirectToNewRoom(this.id)'>#" + roomnames + "</div>";
+//End code
+});});}
+getData();
+function redirectToNewRoom(anyroom){
+    console.log(anyroom);
+    localStorage.setItem("Roomname", anyroom);
+    window.location = "kwitter_page.html";
+    }
